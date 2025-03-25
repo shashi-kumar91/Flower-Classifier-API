059e371b311039a5e531584af502aa83806eddcf
shashi-kumar91 <132143332+shashi-kumar91@users.noreply.github.com>
2025-03-26 00:43:35 +0530

    app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ DRIVE_FILE_ID = "1pZ_DJIpa9YXSxB32rASBYw-VuFjgciY2"
 # Function to download model if not available locally
 def download_model():
     """Download the model from Google Drive if not present"""
-    url = f"https://drive.google.com/uc?export=download&id={DRIVE_FILE_ID}"
+    url = f"https://drive.google.com/uc?id={DRIVE_FILE_ID}"
     logger.info("Downloading model from Google Drive...")
     try:
         gdown.download(url, MODEL_PATH, quiet=False)
@@ -36,28 +36,6 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 # Global model variable
 model = None
-
-# Class names
-class_names = [
-    'pink primrose', 'hard-leaved pocket orchid', 'canterbury bells', 'sweet pea',
-    'english marigold', 'tiger lily', 'moon orchid', 'bird of paradise', 'monkshood',
-    'globe thistle', 'snapdragon', "colt's foot", 'king protea', 'spear thistle',
-    'yellow iris', 'globe-flower', 'purple coneflower', 'peruvian lily', 'balloon flower',
-    'giant white arum lily', 'fire lily', 'pincushion flower', 'fritillary', 'red ginger',
-    'grape hyacinth', 'corn poppy', 'prince of wales feathers', 'stemless gentian', 'artichoke',
-    'sweet william', 'carnation', 'garden phlox', 'love in the mist', 'mexican aster',
-    'alpine sea holly', 'ruby-lipped cattleya', 'cape flower', 'great masterwort', 'siam tulip',
-    'lenten rose', 'barberton daisy', 'daffodil', 'sword lily', 'poinsettia', 'bolero deep blue',
-    'wallflower', 'marigold', 'buttercup', 'oxeye daisy', 'common dandelion', 'petunia',
-    'wild pansy', 'primula', 'sunflower', 'pelargonium', 'bishop of llandaff', 'gaura', 'geranium',
-    'orange dahlia', 'pink-yellow dahlia', 'cautleya spicata', 'japanese anemone', 'black-eyed susan',
-    'silverbush', 'californian poppy', 'osteospermum', 'spring crocus', 'bearded iris', 'windflower',
-    'tree poppy', 'gazania', 'azalea', 'water lily', 'rose', 'thorn apple', 'morning glory',
-    'passion flower', 'lotus lotus', 'toad lily', 'anthurium', 'frangipani', 'clematis', 'hibiscus',
-    'columbine', 'desert-rose', 'tree mallow', 'magnolia', 'cyclamen', 'watercress', 'canna lily',
-    'hippeastrum', 'bee balm', 'ball moss', 'foxglove', 'bougainvillea', 'camellia', 'mallow',
-    'mexican petunia', 'bromelia', 'blanket flower', 'trumpet creeper', 'blackberry lily'
-]
 
 # Load model at startup
 @app.on_event("startup")
@@ -80,7 +58,7 @@ def predict_class(image: Image.Image, model):
     image = tf.image.resize(image, [180, 180])
     image = np.expand_dims(image, axis=0)
     prediction = model.predict(image)
-    return class_names[np.argmax(prediction)]
+    return "Prediction Result"
 
 # Prediction endpoint
 @app.post("/predict")
